69c9358d89ccf08f9329c7043874ee98258d6e51
mkawi <mkawi@github.com>
2023-11-20 13:52:57 +0000

    Add /api/topics GET route, including topic controller, model and integration test

--- __tests__/routes.test.js
+++ __tests__/routes.test.js
@@ -1,5 +1,31 @@
 const request = require("supertest");
+const app = require("../app");
 
-describe("", () => {
-	test("", () => {});
+const db = require("../db/connection");
+const testData = require("../db/data/test-data");
+const seed = require("../db/seeds/seed");
+
+beforeEach(() => seed(testData));
+afterAll(() => db.end());
+
+describe("GET: /api/topics", () => {
+	test("200: sucessfully responds with an array of topic objects", () => {
+		return request(app)
+			.get("/api/topics")
+			.expect(200)
+			.expect("Content-Type", /json/)
+			.then(({ body: { topics } }) => {
+				expect(topics.length).toBe(3);
+
+				topics.forEach((topic) => {
+					expect(topic).toMatchObject({
+						description: topic.description,
+						slug: topic.slug,
+					});
+
+					expect(typeof topic.description).toBe("string");
+					expect(typeof topic.slug).toBe("string");
+				});
+			});
+	});
 });
--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const express = require("express");
 const app = express();
 
+const { getTopics } = require("./controllers/topic.controller");
+
+app.get("/api/topics", getTopics);
+
 module.exports = app;
