724f1ad7cec3ffa1abbcbec435d48e37cc1fdef8
mkawi <mkawi@github.com>
2023-11-21 11:53:25 +0000

    Add error handling for PostgresQL errors: bad requests

--- controllers/error.handlers.js
+++ controllers/error.handlers.js
@@ -5,14 +5,18 @@ exports.handle404CatchAll = (req, res) => {
 
 exports.handleCustomErrors = (err, req, res, next) => {
 	if (err.status) {
-		res.status(err.status).send({ msg: err.msg });
+		res.status(err.status).send(err);
 	} else {
 		next(err);
 	}
 };
 
 exports.handlePsqlErrors = (err, req, res, next) => {
-	next(err);
+	if (err.code === "22P02") {
+		res.status(400).send({ msg: "Bad Request!" });
+	} else {
+		next(err);
+	}
 };
 
 exports.handleServerErrors = (err, req, res, next) => {
