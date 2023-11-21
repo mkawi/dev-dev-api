dc19abe4a9221420b7d31e5250618eb5a4158d60
mkawi <mkawi@github.com>
2023-11-21 15:03:48 +0000

    refactor selectAllArticles query to return comment_count as an integer, removing the need to map the returned rows and manually convert it into a Number

--- models/article.model.js
+++ models/article.model.js
@@ -5,16 +5,14 @@ exports.selectAllArticles = () => {
 		.query(
 			`SELECT articles.article_id, articles.title, articles.author, 
                 articles.topic, articles.created_at, articles.votes, 
-                article_img_url, COUNT(comment_id) AS comment_count 
+                article_img_url, COUNT(comment_id)::int AS comment_count 
             FROM articles
             LEFT JOIN comments ON comments.article_id = articles.article_id
             GROUP BY articles.article_id 
             ORDER BY articles.created_at DESC;`
 		)
 		.then(({ rows }) => {
-			return rows.map((article) => {
-				return { ...article, comment_count: Number(article.comment_count) };
-			});
+			return rows;
 		});
 };
 
