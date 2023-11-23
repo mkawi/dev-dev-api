f141e508c2f6b60455518c96975966a86d4ebef7
mkawi <mkawi@github.com>
2023-11-23 11:49:48 +0000

    Add additional integration test to check if /api/articles/:article_id GET route returns an article with the correct comment_count property

--- __tests__/routes.test.js
+++ __tests__/routes.test.js
@@ -94,7 +94,27 @@ describe("GET: /api/articles/:article_id", () => {
 					votes: 0,
 					article_img_url:
 						"https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
-					comment_count: 2,
+				});
+			});
+	});
+
+	test("200: successfully responds with a single article with an additional comment_count property", () => {
+		return request(app)
+			.get("/api/articles/1")
+			.expect(200)
+			.expect("Content-Type", /json/)
+			.then(({ body: { article } }) => {
+				expect(article).toMatchObject({
+					article_id: 1,
+					title: "Living in the shadow of a great man",
+					topic: "mitch",
+					author: "butter_bridge",
+					body: "I find this existence challenging",
+					created_at: "2020-07-09T20:11:00.000Z",
+					votes: 100,
+					article_img_url:
+						"https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+					comment_count: 11,
 				});
 			});
 	});
